9a04a672c78317df303f9baf62e56e6e063863f3
Devindi-u <panchalidevindi@gmail.com>
2025-11-17 15:42:17 +0530

    popup html and css

---
+++ popup/popup.js
@@ -0,0 +1,13 @@
+//DOM elements
+const elements = {
+    blockingStatus: document.getElementById('blockingStatus'),
+    blockingCount: document.getElementById('blockedCount'),
+    scheduleStatus: document.getElementById('scheduleStatus'),
+    toggleBlocking: document.getElementById('toggleBlocking'),
+    toggleText: document.getElementById('toggleText'),
+    openOption: document.getElementById('openOption'),
+    textBlock: document.getElementById('textBlock'),
+    quickSiteInput: document.getElementById('quickSiteInput'),
+    quickAddBtn: document.getElementById('quickAddBtn'),
+};
+
